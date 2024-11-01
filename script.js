85c4e449b0cb1fc9021fc7b7ef9ee6b5f84cc2db
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-11-02 00:19:04 +0100

    Removed a log

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const scrollElements = document.getElementsByClassName("scroll-block");
 
 window.addEventListener("scroll", () => {
-  console.log(window.scrollY);
   for (const element of scrollElements) {
     element.style.backgroundPositionY = `-${window.scrollY}px`;
   }

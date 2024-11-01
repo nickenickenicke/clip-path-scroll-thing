8f592e05c8d619304f3275ebe1ef5802d3801e48
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-11-02 00:33:18 +0100

    Make everything wierder

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const scrollElements = document.getElementsByClassName("scroll-block");
 
 window.addEventListener("scroll", () => {
-  for (const element of scrollElements) {
-    element.style.backgroundPositionY = `-${window.scrollY}px`;
-  }
+  scrollElements[0].style.backgroundPositionY = `-${window.scrollY}px`;
+  scrollElements[1].style.backgroundPositionY = `${window.scrollY}px`;
+  scrollElements[2].style.backgroundPositionX = `-${window.scrollY}px`;
+  scrollElements[3].style.backgroundPositionX = `${window.scrollY}px`;
+  scrollElements[4].style.backgroundPositionY = `${window.scrollY}px`;
+
+  // for (const element of scrollElements) {
+  //   element.style.backgroundPositionY = `-${window.scrollY}px`;
+  // }
 });
